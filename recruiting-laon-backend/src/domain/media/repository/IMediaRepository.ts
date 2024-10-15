d6f1b06a18f4b233699f3f44bc423fbf5c3ff58c
Matheus <matheuschmidt.b@gmail.com>
2024-10-15 17:56:25 -0300

    🚧 wip

--- recruiting-laon-backend/src/domain/media/repository/IMediaRepository.ts
+++ recruiting-laon-backend/src/domain/media/repository/IMediaRepository.ts
@@ -2,4 +2,9 @@ import type { Media } from "../media";
 
 export interface IMediaRepository {
 	create(media: Media): Promise<Media>;
+	findByName(
+		name: string,
+		page: number,
+		pageSize: number,
+	): Promise<{ items: Media[]; total: number }>;
 }
--- recruiting-laon-backend/src/infrastructure/repository/mediaRepository.ts
+++ recruiting-laon-backend/src/infrastructure/repository/mediaRepository.ts
@@ -1,6 +1,8 @@
 import type { Media } from "@/domain/media/media";
+
 import type { IMediaRepository } from "@/domain/media/repository/IMediaRepository";
 import { createId } from "@paralleldrive/cuid2";
+import { eq, sql } from "drizzle-orm";
 import { db } from "../database/connection";
 import { media as mediaSchema } from "../database/schema";
 
@@ -24,5 +26,31 @@ export class MediaRepository implements IMediaRepository {
 		});
 
 		return media;
+	}
+
+	async findByName(
+		name: string,
+		page: number,
+		pageSize: number,
+	): Promise<{ items: Media[]; total: number }> {
+		const offset = (page - 1) * pageSize;
+
+		const totalResult = await db
+			.select({ count: sql`COUNT(*)` })
+			.from(mediaSchema)
+			.where(eq(mediaSchema.name, name));
+
+		const total = totalResult[0]?.count || 0;
+
+		const query = db
+			.select()
+			.from(mediaSchema)
+			.where(eq(mediaSchema.name, name))
+			.limit(pageSize)
+			.offset(offset);
+
+		const items = await query;
+
+		return { items, total };
 	}
 }
